20a1a315738bb1b423d978153644713cd9b40e75
Seun4c <oluwaseundada700@gmail.com>
2022-09-08 13:47:07 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
 * main - prints a text
 *
-* Return: Always 1 (success)
+* Return: Always 1 (Success)
 */
-int main(void)
+int main (void)
 {
-	write(2, "and that piece of art is useful\" - Dora Karpar, 2015-10-19\n", 59);
-	return (1);
+	write(2, "and that piece of art is useful" - Dora Karpar, 2015-10-19\n", 59);
+	return (1)
 }
