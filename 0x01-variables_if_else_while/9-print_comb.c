917636278f500b57814760bbc2981993b154a0db
Seun4c <oluwaseundada700@gmail.com>
2022-09-09 20:16:18 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Prints a series of numbers with commas
  *
@@ -12,7 +11,7 @@ int main(void)
 	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
-		
+
 		if (c != '9')
 		{
 			putchar(',');
