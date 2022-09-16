96674dd739bdd144f9eca76b9c16b2448e31b45d
Seun4c <oluwaseundada700@gmail.com>
2022-09-15 17:42:27 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - prints the numbers from 1 to 100, followed by a new line
@@ -27,6 +28,10 @@ int main(void)
 		else if (i == 1)
 		{
 			printf("%d", i);
+		}
+		else
+		{
+			printf(" %d", i);
 		}
 	}
 	printf("\n");
